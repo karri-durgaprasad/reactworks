94dbcfd7013b1ac2f5b9eae1de62325deb03f0e1
padmaboddu <padmaboddu062@gmail.com>
2024-11-04 12:57:32 +0530

    new

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
     <div style={{height:'100vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
     <div style={{backgroundColor:'lightblue'}}>
         <h3 style={{textAlign:'center'}}>Login</h3>
-        <input placeholder='enter your name' style={{marginBottom:'10px',textAlign:'center',marginLeft:'35px'}}/><br/>
+        <input placeholder='enter your name' style={{marginBottom:'20px',textAlign:'center',marginLeft:'35px'}}/><br/>
         <input placeholder='enter your password' style={{marginBottom:'10px',textAlign:'center',marginLeft:'35px'}} /><br/>
        <Link to='/dashboard'><button className='btn btn-success' style={{fontSize:'13px',marginLeft:'37%'}}>Login</button></Link><br/>
        <p>you dont have an account?<Link to='signin'><button className='btn btn-outline-primary' style={{fontSize:'13px'}}>SignUp</button></Link></p> 
